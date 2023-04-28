5fe06f152f1ee5536061f7c509ba84efc22c05d7
KP <31988529+katsamapol@users.noreply.github.com>
2023-04-28 08:35:01 -0400

    Update news extractor and analyzer
    - Retrieve and replace 3 days old data

--- fullstack/news-extract/app.py
+++ fullstack/news-extract/app.py
@@ -26,31 +26,39 @@ app = Flask(__name__)
 search_terms = [
     "Apple", "Amgen", "American Express", "Boeing", "Caterpillar",
     "Salesforce",	"Cisco Systems", "Chevron",	"Walt Disney",
-    "Dow ",	"Goldman Sachs Group", "Home Depot",
+    "Dow",	"Goldman Sachs Group", "Home Depot",
     "Honeywell International",	"IBM",
     "Intel", "Johnson & Johnson", "JP Morgan", "Coca-Cola",
     "McDonalds", "3M Company", "Merck & Company",
-    "Microsoft", "Nike", "Procter & Gamble", "The Travelers",	"United Health Group", "Visa ",
-    "Verizon Communications",	"Walgreens", "Walmart "
+    "Microsoft", "Nike", "Procter & Gamble", "The Travelers",	"United Health Group", "Visa",
+    "Verizon Communications",	"Walgreens", "Walmart"
 ]
 ticker_search = [
     "AAPL",	"AMGN",	"AXP",	"BA",	"CAT",	"CRM",	"CSCO",	"CVX",	"DIS",	"DOW",	"GS",	"HD",	"HON",	"IBM",	"INTC",	"JNJ",	"JPM",	"KO",	"MCD",	"MMM",	"MRK",	"MSFT",	"NKE",	"PG",	"TRV",	"UNH",	"V",	"VZ",	"WBA",	"WMT"
 ]
-ticker_company_dict = {}
+ticker_to_company = {}
+company_to_ticker = {}
 for i in range(len(search_terms)):
-    ticker_company_dict[ticker_search[i]] = search_terms[i]
-
+    ticker_to_company[ticker_search[i]] = search_terms[i]
+    company_to_ticker[search_terms[i]] = ticker_search[i]
 
 @app.route("/")
+def index():
+    return { 'result': "News extracting service is running."} 
+
+
+@app.route("/extract")
 def extract_nyt():
     """
     This endpoint is particularly to query the over Stock market news from NYT
     For this, what we'll do is for every news, if there is an entity within our Dow30, we'll also send that
     news to the Companies collection. Otherwise we only send data to Stock Market collections
     """
-    end = (datetime.datetime.today()) - datetime.timedelta(days=1)
+    
+    end = (datetime.datetime.today()) - datetime.timedelta(days=2)
     start = end - datetime.timedelta(days=2)
     start, end = start.strftime('%Y%m%d'), end.strftime('%Y%m%d')
+    # print(start, end)
     search_term = "Stock Market"
     query = f"https://api.nytimes.com/svc/search/v2/articlesearch.json?q={search_term}&begin_date={start}&end_date={end}&page=0&api-key={KEY}"
     res = requests.get(query)
@@ -62,7 +70,8 @@ def extract_nyt():
         date = formatDate(doc['pub_date'])
         newsNER = extractFromNews(doc['lead_paragraph'])
         news_data = {
-            'date': date, 'news': newsNER[0], 'entities': newsNER[1], 'search_term': search_term, 'source': 'nyt'}
+            # 'ticker': company_to_ticker[newsNER[1]],
+            'date': date, 'news': newsNER[0], 'entities': newsNER[1], 'search_term': search_term, 'source': 'nyt', 'symbol':''}
         stock_market_data.append(news_data)
     try:
         collection.insert_many(list(stock_market_data))
@@ -82,7 +91,7 @@ def extract_nyt_main():
     For this, what we'll do is for every news, if there is an entity within our Dow30, we'll also send that
     news to the Companies collection. Otherwise we only send data to Stock Market collections
     """
-    end = (datetime.datetime.today()) - datetime.timedelta(days=1)
+    end = (datetime.datetime.today()) - datetime.timedelta(days=2)
     start = end - datetime.timedelta(days=2)
     start, end = start.strftime('%Y%m%d'), end.strftime('%Y%m%d')
 
@@ -95,19 +104,23 @@ def extract_nyt_main():
         response = res.json()
 
         stock_market_data = []
-        print(search_term)
         if 'response' in response.keys():
+            print(search_term)
             for doc in response['response']['docs']:
                 date = formatDate(doc['pub_date'])
                 newsNER = extractFromNews(doc['lead_paragraph'])
                 news_data = {
-                    'date': date, 'news': newsNER[0], 'entities': newsNER[1], 'search_term': search_term, 'source': 'nyt'}
+                    'date': date, 'news': newsNER[0], 'entities': newsNER[1], 'search_term': search_term, 'source': 'nyt', 'symbol':company_to_ticker[search_term]}
+                
                 stock_market_data.append(news_data)
+                # print(stock_market_data)
+
             try:
                 collection.insert_many(list(stock_market_data))
                 print("success")
             except:
-                print("error")
+                pass
+                print("error (nothing to insert)")
 
     return {
         "message": "success"
@@ -116,7 +129,7 @@ def extract_nyt_main():
 
 @app.route("/mediastack-gen")
 def mediastackGeneral():
-    end = (datetime.datetime.today()) - datetime.timedelta(days=0)
+    end = (datetime.datetime.today()) - datetime.timedelta(days=2)
     start = end - datetime.timedelta(days=2)
     start_end = f"{start.strftime('%Y-%m-%d')},{end.strftime('%Y-%m-%d')}"
     params = urllib.parse.urlencode({
@@ -135,12 +148,16 @@ def mediastackGeneral():
     for news in response['data']:
         #news_data['date'] = news['published_at'].strftime('%m-%d-%Y')
         newsNER = extractFromNews(news['title'])
+
         news_data = {
+            # 'ticker': company_to_ticker[newsNER[1]],
             'date': formatDate(news['published_at']),
             'news': newsNER[0], 'entities': newsNER[1],
-            'search_term': 'Stock Market', 'source': 'mediastack'
+            'search_term': 'Stock Market', 'source': 'mediastack', 'symbol':'',
         }
+        # print(news_data)
         stock_market_data.append(news_data)
+        
     try:
         collection.insert_many(list(stock_market_data))
         return {
@@ -162,7 +179,7 @@ def extract_yfinance():
             news_data = {
                 'date': datetime.datetime.fromtimestamp(news['providerPublishTime']).strftime("%m-%d-%Y"),
                 'news': newsNER[0], 'entities': newsNER[1],
-                'search_term': ticker_company_dict[ticker], 'source': 'yfinance'
+                'search_term': ticker_to_company[ticker], 'source': 'yfinance', 'symbol': ticker
             }
         stock_market_data.append(news_data)
     try:
@@ -188,7 +205,7 @@ def getnews(q_ticker=None):
         json_result.append({'date': row['date'], 'data': row['data']})
     print(json_result)
     
-    return json.dumps(json_result)
+    return json.dumps(json_result[1])
 
 
 @app.route("/test")
@@ -200,29 +217,25 @@ def test():
 @app.cli.command('db_seed')
 @app.route("/seed")
 def seed():
+
     # lets start with the extractionDB
-    # or MongoClient("localhost:27")
     client = MongoClient('news-extract-mongo', 27019)
     db = client.test_db
-    # db.list_collection_names()  # this should be empty
-
-    # drop collection if previous cell wasn't empty
-    for collection in db.list_collection_names():
-        db[collection].drop()
-
-    # lets now repeat for sentimentDB
-    # or MongoClient("localhost:27")
-    client = MongoClient('news-sentiment-analysis-mongo', 27018)
-    db = client.test_db
-    # db.list_collection_names()  # this should be empty
-
-    # drop collection if previous cell wasn't empty
-    for collection in db.list_collection_names():
-        db[collection].drop()
-    # db.list_collection_names()  # this should be empty
+    
+    end = (datetime.datetime.today()) - datetime.timedelta(days=2)
+    between = end - datetime.timedelta(days=1)
+    start = between - datetime.timedelta(days=1)
+    
+    dates = [start.strftime('%m-%d-%Y'), between.strftime('%m-%d-%Y'), end.strftime('%m-%d-%Y')]
+    collection = db.test
+    for dd in dates:
+        collection.delete_many({'date': dd})
+    collection = db.transformedData
+    for dd in dates:
+        collection.delete_many({'date': dd})
 
     endpoints = [
-        '/',
+        '/extract',
         '/nyt',
         '/mediastack-gen',
         '/yfinance'
@@ -233,12 +246,19 @@ def seed():
         response = requests.get(url)
         print(f"{endpt} - {response.json()}")
 
+
+    client2 = MongoClient('news-sentiment-analysis-mongo', 27018)
+    db2 = client2.test_db
+    collection2 = db2.test
+    for dd in dates:
+        collection2.delete_many({'date': dd})
+
     # make sure the sentiment-analysis flask app is listening on port 8002
     url = 'http://news-sentiment-analysis-flask:8002/transform'
 
     response = requests.get(url)
 
-    print(response.json())
+    print(response)
 
     # call endpoint to run sentiment
     # make sure the extraction flask app is listening on port 8001
@@ -246,13 +266,13 @@ def seed():
 
     response = requests.get(url)
 
-    print(response.json())
+    print(response)
 
     # Test access data
     # connect to sentiment db
     
-    client = MongoClient('news-sentiment-analysis-mongo', 27018)
-    db = client.test_db
+    client2 = MongoClient('news-sentiment-analysis-mongo', 27018)
+    db = client2.test_db
     collection = db.test
     for doc in collection.find():
         print(doc)
--- fullstack/news-sentiment-analysis/app.py
+++ fullstack/news-sentiment-analysis/app.py
@@ -19,40 +19,56 @@ def transform_data():
     db = client.test_db
     collection = db.test
     #load data from yesterday
-    yesterday = ((datetime.datetime.today()) - datetime.timedelta(days=1)).strftime('%m-%d-%Y')
-    query = {
-        'date': {'$eq': yesterday}
-    }
-    data = collection.find(query, {'_id':0})
-    data_to_transform = []
-    #initialize new collection in extraction db for transformed data
-    db = client.test_db
-    collection = db.transformedData
-    for t in data:
-        """
-        There are two kinds of data: 1. Stock Market, 2. Company Specifics
-        For stock market data, we take as is. 
-        For company specific:
-        1. For ones coming in from yfinance, we take as is
-        2. For both nyt and mediastack, we will only pick the docs where theres a match between search_term and entities.
-            That is because, a lot of what is queried from these two sources may not contain what we are looking for.
-        """
-        if t['source'] == 'yfinance':
-            data_to_transform.append(t)
-        else:
-            if t['search_term'] in t['entities'] and t['search_term'] != "Stock Market":
-                data_to_transform.append(t)
-            elif t['search_term'] == "Stock Market":
-                data_to_transform.append(t)
-    try:
-        collection.insert_many( list(data_to_transform) )
-        return {
-            'message': 'Successfully pushed transformed data to mongo'
+    end = (datetime.datetime.today()) - datetime.timedelta(days=2)
+    between = end - datetime.timedelta(days=1)
+    start = between - datetime.timedelta(days=1)
+    
+    dates = [start.strftime('%m-%d-%Y'), between.strftime('%m-%d-%Y'), end.strftime('%m-%d-%Y')]
+    # for dd in dates:
+    #     collection.delete_many({'date': dd})
+
+    # yesterday = ((datetime.datetime.today()) - datetime.timedelta(days=1)).strftime('%m-%d-%Y')
+    results = []
+    for dd in dates:
+        query = {
+            'date': dd
         }
-    except:
-        return {
-            'message': 'Error pushing transformed data.'
-        }
+        print(f"transform {dd}")
+        collection = db.test
+        data = collection.find(query, {'_id':0})
+        data_to_transform = []
+        #initialize new collection in extraction db for transformed data
+        collection = db.transformedData
+        for t in data:
+            print(t)
+            """
+            There are two kinds of data: 1. Stock Market, 2. Company Specifics
+            For stock market data, we take as is. 
+            For company specific:
+            1. For ones coming in from yfinance, we take as is
+            2. For both nyt and mediastack, we will only pick the docs where theres a match between search_term and entities.
+                That is because, a lot of what is queried from these two sources may not contain what we are looking for.
+            """
+            if t['source'] == 'yfinance':
+                data_to_transform.append(t)
+            else:
+                if t['search_term'] in t['entities'] and t['search_term'] != "Stock Market":
+                    data_to_transform.append(t)
+                elif t['search_term'] == "Stock Market":
+                    data_to_transform.append(t)
+        try:
+            collection.insert_many( list(data_to_transform) )
+            # return {
+            #     'message': 'Successfully pushed transformed data to mongo'
+            # }
+            results.append(f"{dd} Successfully pushed transformed data to mongo.'")
+        except:
+            # return {
+            #     'message': 'Error pushing transformed data.'
+            # }
+            results.append(f"{dd} Error pushing transformed data.'")
+
+    return results
 
 @app.route("/sentiment")
 def hello_world():
@@ -60,57 +76,72 @@ def hello_world():
     # client = MongoClient('mongodb://localhost:27017') #connect to extraction db
     db = client.test_db
     collection = db.transformedData
-    yesterday = ((datetime.datetime.today()) - datetime.timedelta(days=1)).strftime('%m-%d-%Y')
-    query = {
-        'date': {'$eq': yesterday}
-    }
-    test = collection.find(query)
-    transformed_data = []
-    for t in test:
-        transformed_data.append(t)
+    end = (datetime.datetime.today()) - datetime.timedelta(days=2)
+    between = end - datetime.timedelta(days=1)
+    start = between - datetime.timedelta(days=1)
     
-    data  = defaultdict(dict)
-    for i in range(len(transformed_data)):
-        if transformed_data[i]['date'] not in data:
-            data[ transformed_data[i]['date'] ] = {}
-        if transformed_data[i]['search_term'] not in data[ transformed_data[i]['date'] ]:
-            data[ transformed_data[i]['date'] ][ transformed_data[i]['search_term'] ] = {
-                'avg_sentiment': [],
-                'news': []
-            }
+    dates = [start.strftime('%m-%d-%Y'), between.strftime('%m-%d-%Y'), end.strftime('%m-%d-%Y')]
 
-        inputs = tokenizer(transformed_data[i]['news'], padding = True, truncation = True, return_tensors='pt')
-        outputs = model(**inputs)
-        predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)
-
-        positive = predictions[:, 0].tolist()
-        negative = predictions[:, 1].tolist()
-        neutral = predictions[:, 2].tolist()
-        sentiment_data = {'Headline':transformed_data[i]['news'],
-            'sentiment': {
-                'positive': positive[0],
-                'negative': negative[0],
-                'neutral': neutral[0] 
-            },
-            'source': transformed_data[i]['source']
+    # yesterday = ((datetime.datetime.today()) - datetime.timedelta(days=1)).strftime('%m-%d-%Y')
+    results = []
+    for dd in dates:
+        print(f"sentiment {dd}")
+        query = {
+            'date': dd
         }
-        data[ transformed_data[i]['date'] ][ transformed_data[i]['search_term'] ]['news'].append(sentiment_data)
+        test = collection.find(query)
+        transformed_data = []
+        for t in test:
+            transformed_data.append(t)
+        
+        data  = defaultdict(dict)
+        print(transformed_data)
+        for i in range(len(transformed_data)):
+            if transformed_data[i]['date'] not in data:
+                data[ transformed_data[i]['date'] ] = {}
 
-    #connect and send data to sentiment store db
-    client2 = MongoClient('news-sentiment-analysis-mongo', 27018)
-    # client2 = MongoClient('mongodb://localhost:27018') 
-    db2 = client2.test_db
-    collection2 = db2.test
-    try:
-        collection2.update_one(
-            {'date': yesterday},
-            {'$set': {'data': data}},
-            upsert=True
-        )
-        return {"message":"Data push to sentiment DB- Succeded"}
-    except:
-        return {"message":"Data push to sentiment DB failed"}
+            if transformed_data[i]['search_term'] not in data[ transformed_data[i]['date'] ]:
+                data[ transformed_data[i]['date'] ][ transformed_data[i]['search_term'] ] = {
+                    'avg_sentiment': [],
+                    'news': []
+                }
 
+            inputs = tokenizer(transformed_data[i]['news'], padding = True, truncation = True, return_tensors='pt')
+            outputs = model(**inputs)
+            predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)
+
+            positive = predictions[:, 0].tolist()
+            negative = predictions[:, 1].tolist()
+            neutral = predictions[:, 2].tolist()
+            sentiment_data = {
+                'symbol':transformed_data[i]['symbol'],
+                'headline':transformed_data[i]['news'],
+                'sentiment': {
+                    'positive': positive[0],
+                    'negative': negative[0],
+                    'neutral': neutral[0] 
+                },
+                'source': transformed_data[i]['source']
+            }
+            data[ transformed_data[i]['date'] ][ transformed_data[i]['search_term'] ]['news'].append(sentiment_data)
+
+        #connect and send data to sentiment store db
+        client2 = MongoClient('news-sentiment-analysis-mongo', 27018)
+        # client2 = MongoClient('mongodb://localhost:27018') 
+        db2 = client2.test_db
+        collection2 = db2.test
+        try:
+            collection2.update_one(
+                {'date': dd},
+                {'$set': {'data': data}},
+                upsert=True
+            )
+            # return {"message":"Data push to sentiment DB- Succeded"}
+            results.append(f"{dd} Data push to sentiment DB Succeded'")
+        except:
+            # return {"message":"Data push to sentiment DB failed"}
+            results.append(f"{dd} Data push to sentiment DB failed'")
+    return results
 
 
 if __name__ == '__main__':
